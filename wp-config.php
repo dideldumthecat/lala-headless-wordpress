f1a1c0032052d2ef51c92c180e0d9a19bc529652
Alexander Summa <info@alexandersumma.de>
2024-12-25 20:20:47 +0100

    Disable WP-Cron due to performance issues and use a real cron job instead

--- wp-config.php
+++ wp-config.php
@@ -55,6 +55,9 @@ define( 'WPMS_SMTP_PASS', $_ENV['WPMS_SMTP_PASS'] );
 // Disable the plugin and theme editor
 define( 'DISALLOW_FILE_EDIT', true );
 
+// Disable WP cron due to performance issues and use a real cron job instead
+define('DISABLE_WP_CRON', true);
+
 /**
  * Set WordPress Database Table prefix if not already set.
  *
